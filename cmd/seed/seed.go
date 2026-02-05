9ff7b625c9cdd77343f7423ecfbe05d800e4f245
darkvadr <glowburn.acc@gmail.com>
2026-02-05 17:11:32 +0000

    feat: add seed functionality to populate database with fake users and posts

---
+++ cmd/seed/seed.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"log"
+	mrand "math/rand"
+
+	"github.com/d4rthvadr/dusky-go/internal/config"
+	"github.com/d4rthvadr/dusky-go/internal/db"
+	"github.com/d4rthvadr/dusky-go/internal/models"
+
+	"github.com/d4rthvadr/dusky-go/internal/store"
+	"github.com/joho/godotenv"
+)
+
+var fakeUsernames = []string{
+	"alice",
+	"bob",
+	"charlie",
+	"dave",
+	"eve",
+	"frank",
+}
+
+func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	config, err := config.InitializeConfig()
+	if err != nil {
+		log.Fatal("Error initializing config:", err)
+	}
+
+	db, err := db.New(config.Db.Addr, config.Db.MaxOpenConns, config.Db.MaxIdleConns, config.Db.MaxIdleTime)
+	if err != nil {
+		log.Panic("Error connecting to the database:", err)
+	}
+
+	store := store.NewStorage(db)
+	if err = Seed(store); err != nil {
+		fmt.Println("Error seeding data:", err)
+	}
+}
+
+func Seed(store store.Storage) error {
+	fmt.Println("seeding...")
+	ctx := context.Background()
+
+	users := generateUsers(3)
+
+	userIds := make([]int64, len(users))
+
+	for index, user := range users {
+		if err := store.Users.Create(ctx, &user); err != nil {
+			return fmt.Errorf("error creating user: %w", err)
+		}
+		userIds[index] = user.ID
+	}
+
+	posts := generatePosts(200, userIds)
+
+	for _, post := range posts {
+		if err := store.Posts.Create(ctx, &post); err != nil {
+			return fmt.Errorf("error creating post: %w", err)
+		}
+	}
+
+	fmt.Println("done")
+
+	return nil
+}
+
+func getFakeUsername(i int) string {
+	return fakeUsernames[i%len(fakeUsernames)] + fmt.Sprintf("%d", i+1)
+}
+
+// generateRandomHash generates a random 4-character hexadecimal string
+func generateRandomHash() string {
+	bytes := make([]byte, 10) // 10 bytes will give us a 20-character hex string
+	rand.Read(bytes)
+	return hex.EncodeToString(bytes)
+}
+
+func generateUsers(count int) []models.User {
+	users := make([]models.User, count)
+
+	for i := 0; i < count; i++ {
+		username := getFakeUsername(i) + "_" + generateRandomHash()
+		users[i] = models.User{
+			Username: username,
+			Email:    fmt.Sprintf("user_%s@example.com", username),
+			Password: "password123",
+		}
+	}
+	return users
+}
+
+func generatePosts(count int, userIDs []int64) []models.Post {
+	posts := make([]models.Post, count)
+	for i := 0; i < count; i++ {
+		userID := userIDs[mrand.Intn(len(userIDs))]
+		posts[i] = models.Post{
+			Title:   fmt.Sprintf("Post Title %d", i+1),
+			Content: fmt.Sprintf("This is the content of post %d.", i+1),
+			UserID:  userID,
+			Tags:    []string{"tag1", "tag2"},
+		}
+	}
+	return posts
+}
--- internal/store/users.go
+++ internal/store/users.go
@@ -14,7 +14,7 @@ type UserStore struct {
 func (u *UserStore) Create(ctx context.Context, user *models.User) error {
 
 	query := `
-	INSERT INTO users (username, email, password) 
+	INSERT INTO users (username, email, password_hash) 
 	VALUES ($1, $2, $3) RETURNING id, created_at, updated_at
 	`
 	err := u.db.QueryRowContext(ctx, query, user.Username, user.Email, user.Password).
