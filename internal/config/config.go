31bb3b8c088c84da4a7fe09d2a1e00a1d07e9c08
darkvadr <glowburn.acc@gmail.com>
2026-02-13 08:57:32 +0000

    feat: add API_URL configuration to support dynamic API endpoint

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"time"
 
 	env "github.com/d4rthvadr/dusky-go/internal/utils"
@@ -21,15 +22,17 @@ type DbConfig struct {
 type AppConfig struct {
 	Server ServerConfig
 	Db     DbConfig
+	ApiUrl string
 }
 
 func InitializeConfig() (*AppConfig, error) {
 
-	serverAddr := env.GetEnv("ADDR", "3000")
+	serverAddr := env.GetEnv("ADDR", "8082")
 	dbAddr := env.GetEnv("DB_ADDR", "postgres://admin:adminpassword@localhost:5432/duskydb?sslmode=disable")
 	maxOpenConns := env.GetEnvAsInt("DB_MAX_OPEN_CONNS", 30)
 	maxIdleConns := env.GetEnvAsInt("DB_MAX_IDLE_CONNS", 30)
 	maxIdleTime := env.GetEnvAsDuration("DB_MAX_IDLE_TIME", time.Minute*15)
+	apiUrl := env.GetEnv("API_URL", fmt.Sprintf("http://localhost:%s/v1", serverAddr))
 
 	config := &AppConfig{
 		Server: ServerConfig{
@@ -41,6 +44,7 @@ func InitializeConfig() (*AppConfig, error) {
 			MaxIdleConns: maxIdleConns,
 			MaxIdleTime:  maxIdleTime,
 		},
+		ApiUrl: apiUrl,
 	}
 	return config, nil
 }
